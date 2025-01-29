1b320a54d2d972ca37b5e9ba993b580d212b0050
Mark Mitchell <markmitchell451@gmail.com>
2025-01-29 13:59:52 +0000

    x orig

--- src/main/java/com/mm/user/config/CorsConfig.java
+++ src/main/java/com/mm/user/config/CorsConfig.java
@@ -2,23 +2,56 @@ package com.mm.user.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
 
+    // @Bean
+    // public WebMvcConfigurer corsConfigurer() {
+    //     return new WebMvcConfigurer() {
+    //         @Override
+    //         public void addCorsMappings(CorsRegistry registry) {
+    //             registry.addMapping("/**")
+    //                     .allowedOrigins("https://app.camelcase.club", "http://localhost:3000") // Add allowed origins
+    //                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+    //                     .allowedHeaders("*")
+    //                     .allowCredentials(true) // Include this if you need credentials
+    //                     .exposedHeaders("Authorization"); // Optional: Expose specific headers to the client
+    //         }
+    //     };
+    // }
+
+    // Default profile (localhost:3000)
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    @Profile("default")
+    public WebMvcConfigurer defaultCorsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://app.camelcase.club", "http://localhost:3000") // Add allowed origins
+                        .allowedOrigins("http://localhost:3000") // Default local origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true) // Include this if you need credentials
-                        .exposedHeaders("Authorization"); // Optional: Expose specific headers to the client
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+    // Demo profile (https://app.camelcase.club)
+    @Bean
+    @Profile("demo")
+    public WebMvcConfigurer demoCorsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://app.camelcase.club") // Demo origin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
