71900e99ba28a0d0440eb8db6a6d370c58b57ff4
Mark Mitchell <markmitchell451@gmail.com>
2025-02-02 10:20:05 +0000

    secure api

--- src/main/java/com/mm/user/config/CorsFilter.java
+++ src/main/java/com/mm/user/config/CorsFilter.java
@@ -18,7 +18,8 @@ public class CorsFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
 
-        res.setHeader("Access-Control-Allow-Origin", "https://app.camelcase.club");
+        //res.setHeader("Access-Control-Allow-Origin", "https://app.camelcase.club");
+        res.setHeader("Access-Control-Allow-Origin", "*");
         res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
         res.setHeader("Access-Control-Allow-Credentials", "true");
