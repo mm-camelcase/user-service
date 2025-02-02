4166f7fec02f1c92e14bc3cf41aa9e7242cfff11
Mark Mitchell <markmitchell451@gmail.com>
2025-02-02 14:00:10 +0000

    secure api

--- src/main/java/com/mm/user/config/CorsConfig.java
+++ src/main/java/com/mm/user/config/CorsConfig.java
@@ -32,10 +32,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // Default local origin
+                        .allowedOrigins("*") // local app
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(false);
             }
         };
     }
